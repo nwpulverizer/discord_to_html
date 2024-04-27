44c303b6bf470834faa2adde6170e2ac54e5c2ae
nwpulverizer <nathan.w.pulver@gmail.com>
2024-04-26 20:36:11 -0700

    better classes for accessing forum posts.

--- src/HTML.py
+++ src/HTML.py
@@ -4,6 +4,7 @@ import markdown
 
 
 Post = ForwardRef("Post")
+templates_path = Path("/home/nathan/Documents/boot.dev/discord_to_html/templates/")
 
 
 class Reply:
@@ -12,7 +13,7 @@ class Reply:
         self.username = username
         self.timestamp = timestamp
         self.parent = parent
-        self.template = Path("../templates/replycard.html")
+        self.template = templates_path / "replycard.html"
 
     def to_html(self):
         with open(self.template, "r") as f:
@@ -20,44 +21,72 @@ class Reply:
         html_content = markdown.markdown(self.content)
         self.html = (
             html.replace("{{reply content}}", html_content)
-            .replace("{{Username}}", self.username)
-            .replace("{{timestamp}}", self.timestamp)
+            .replace("{{Username}}", str(self.username))
+            .replace("{{timestamp}}", self.timestamp.strftime("%c"))
         )
 
 
 class Post:
     def __init__(
-        self, replies: List, title: str, username: str, content: str, timestamp
+        self,
+        title: str,
+        username: str,
+        timestamp,
+        replies: List[Reply] = [],
     ):
         self.replies = replies
         self.title: str = title
         self.poster: str = username
         self.timestamp = timestamp
-        self.content: str = content
-        self.template = Path("../templates/thread.html")
+        self.thread_template = templates_path / "thread.html"
+        self.post_card_template = templates_path / "threadcard.html"
 
     # How can I make sure these are sorted by time?
     def to_html(self):
-        with open(self.template, "r") as f:
-            html = f.read()
-        html_content = markdown.markdown(self.content)
+        with open(self.thread_template, "r") as f:
+            thread_html = f.read()
+        with open(self.post_card_template, "r") as f:
+            card_html = f.read()
         all_replies = ""
-        for reply in self.replies:
-            reply.to_html()
-            all_replies += reply.html
-        self.html = (
-            html.replace("{{Thread title}}", self.title)
-            .replace("{{Thread content}}", html_content)
-            .replace("{{Thread replies}}", all_replies)
+        if self.replies is not []:
+            for reply in sorted(self.replies, key=lambda x: x.timestamp):
+                reply.to_html()
+                all_replies += reply.html
+        else:
+            print("No replies.")
+        self.thread_html = thread_html.replace("{{Thread title}}", self.title).replace(
+            "{{Thread replies}}", all_replies
+        )
+        self.thread_card_html = (
+            card_html.replace("{{post-title}}", self.title)
+            .replace("{{Username}}", str(self.poster))
+            .replace("{{timestamp}}", self.timestamp.strftime("%c"))
+            # TODO: Replace {{thread page}} with correct href in template.
+            # must decide naming for threads.
         )
 
 
 class Forum:
-    def __init__(self, posts: Optional[List[Post]] = None, title: Optional[str] = None):
-        self.posts: Optional[List[Post]] = posts
-        self.title: Optional[str] = title
+    def __init__(self, posts: List[Post] = [], title: Optional[str] = None):
+        self.posts = posts
+        self.title = title
+        self.template = templates_path / "channel.html"
+
+    def to_html(self):
+        with open(self.template, "r") as f:
+            html = f.read()
+        all_posts = ""
+        if self.posts is not []:
+            for post in sorted(self.posts, key=lambda x: x.timestamp):
+                post.to_html()
+                all_posts += post.thread_card_html
+        else:
+            print(f"No Posts. in {self.title}")
+        self.html = html.replace("{{Channel name}}", self.title).replace(
+            "{{Threads}}", all_posts
+        )
 
 
 class Guild:
-    def __init__(self, forums: List[Forum]):
+    def __init__(self, forums: List[Forum] = []):
         self.forums = forums
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import discord
 from dotenv import load_dotenv
 import os
+from HTML import Reply, Post, Forum, Guild
 
 load_dotenv()
 key = os.getenv("BOT_KEY")
@@ -15,11 +16,23 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f"We have logged in as {client.user}")
     for guild in client.guilds:
+        this_guild = Guild()
         for forum in guild.forums:
+            this_forum = Forum(
+                title=forum.name,
+            )
             for thread in forum.threads:
+                # I don't really know what starter message is. It's not the
+                # Thing you put in when you create your thread
                 messages = [message async for message in thread.history()]
+                this_post = Post(thread.name, str(thread.owner), thread.created_at)
+                this_forum.posts.append(this_post)
                 for message in messages:
-                    print(message.content)
+                    reply = Reply(
+                        message.content, message.author, this_post, message.created_at
+                    )
+                    this_post.replies.append(reply)
+            this_forum.to_html()
 
 
 # @client.event
